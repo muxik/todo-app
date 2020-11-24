2172a37bd0dc5ce6ef5baaf0fb32f77e9773b484
muxik <lqjxm666@163.com>
2020-11-24 16:16:41 +0800

    抽离组建

--- src/App.vue
+++ src/App.vue
@@ -2,48 +2,24 @@
   <main>
     <div class="container">
       <h1>欢迎使用MyTodo待办事项</h1>
-      <div class="input-add">
-        <input type="text" name="todo" />
-        <button>
-          <i class="plus"></i>
-        </button>
-      </div>
-      <div class="filters">
-        <span class="filter active">全部</span>
-        <span class="filter">已完成</span>
-        <span class="filter">未完成</span>
-      </div>
-      <div class="todo-list">
-        <div class="todo-item">
-          <label>
-            <input type="checkbox" name="" id="">
-            Todo1
-            <span class="check-button"></span>
-          </label>
-        </div>
-        <div class="todo-item">
-          <label>
-            <input type="checkbox" name="" id="">
-            Todo2
-            <span class="check-button"></span>
-          </label>
-        </div>
-        <div class="todo-item">
-          <label>
-            <input type="checkbox" name="" id="">
-            Todo3
-            <span class="check-button"></span>
-          </label>
-        </div>
-      </div>
+      <TodoAdd />
+      <TodoFilter />
+      <TodoList />
     </div>
   </main>
 </template>
 
 <script>
+import TodoAdd from "./components/TodoAdd";
+import TodoFilter from "./components/TodoFilter";
+import TodoList from "./components/TodoList";
 export default {
   name: "App",
-  components: {},
+  components: {
+    TodoAdd,
+    TodoFilter,
+    TodoList,
+  },
 };
 </script>
 
@@ -77,115 +53,5 @@ h1 {
   margin: 24px 0;
   font-size: 28px;
   color: #414873;
-}
-
-.input-add {
-  position: relative;
-  display: flex;
-  align-items: center;
-}
-.input-add input {
-  padding: 16px 52px 16px 18px;
-  border: none;
-  outline: none;
-  box-shadow: 0 0 24px rgba(0, 0, 0, 0.08);
-  width: 100%;
-  font-size: 16px;
-  color: #626262;
-}
-
-.input-add button {
-  width: 46px;
-  height: 46px;
-  border-radius: 50%;
-  background: linear-gradient(#c0a5f3, #7f95f7);
-  border: none;
-  outline: none;
-
-  color: #fff;
-  position: absolute;
-  right: 0px;
-
-  cursor: pointer;
-}
-
-.input-add .plus {
-  display: block;
-  width: 100%;
-  height: 100%;
-  background: linear-gradient(#fff, #fff), linear-gradient(#fff, #fff);
-  background-size: 50% 2px, 2px 50%;
-  background-position: center;
-  background-repeat: no-repeat;
-}
-
-.filters {
-  display: flex;
-  margin: 24px 2px;
-  color: #c0c2ce;
-  font-size: 14px;
-}
-
-.filters .filter {
-  margin-right: 14px;
-  transition: 0.8s;
-}
-
-.filters .filter.active {
-  color: #6b729c;
-  transform: scale(1.2);
-}
-
-.todo-list {
-  display: grid;
-  row-gap: 14px;
-}
-
-.todo-item {
-  background: #fff;
-  padding: 16px;
-  border-radius: 8px;
-  color: #626262;
-}
-
-.todo-item label {
-  position: relative;
-  display: flex;
-  align-items: center;
-}
-
-.todo-item label span.check-button {
-  position: absolute;
-  top: 0;
-}
-
-.todo-item label span.check-button::before,
-.todo-item label span.check-button::after {
-  content: "";
-  display: block;
-  position: absolute;
-  width: 18px;
-  height: 18px;
-  border-radius: 50%;
-}
-
-.todo-item label span.check-button::before {
-  border: 1px solid #b382f9;
-}
-
-.todo-item label span.check-button::after {
-  transition: 0.4s;
-  background: #b382f9;
-  transform: translate(1px, 1px) scale(0.8);
-  opacity: 0;
-}
-
-.todo-item input {
-  margin-right: 16px;
-  opacity: 0;
-}
-
-.todo-item input:checked + span.check-button::after {
-  opacity: 1;
 }
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="todo-list">
+    <TodoListItem v-for="n in 3" :key="n" />
+  </div>
+</template>
+
+<script>
+import TodoListItem from "./TodoListItem";
+export default {
+  name: "TodoList",
+  components: {
+    TodoListItem,
+  },
+};
+</script>
+
+<style scoped>
+.todo-list {
+  display: grid;
+  row-gap: 14px;
+}
+</style>
