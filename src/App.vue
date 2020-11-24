7fdb0b85c7b44751e9b94d52a7f3705041054ef1
muxik <lqjxm666@163.com>
2020-11-24 15:06:25 +0800

    template 基本结构

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,46 @@
 <template>
 
+  <main>
+    <div class="container">
+      <h1>欢迎使用MyTodo待办事项</h1>
+      <div class="input-add">
+        <input type="text" name="todo" />
+        <button>
+          <i class="plus"></i>
+        </button>
+      </div>
+      <div class="filters">
+        <span class="filter active">全部</span>
+        <span class="filter">已完成</span>
+        <span class="filter">未完成</span>
+      </div>
+      <div class="todo-list">
+        <div class="todo-item">
+          <label>
+            <input type="checkbox" />
+            Todo 1
+          </label>
+          <span class="check-button"></span>
+        </div>
+
+        <div class="todo-item">
+          <label>
+            <input type="checkbox" />
+            Todo 2
+          </label>
+          <span class="check-button"></span>
+        </div>
+
+        <div class="todo-item">
+          <label>
+            <input type="checkbox" />
+            Todo 3
+          </label>
+          <span class="check-button"></span>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
